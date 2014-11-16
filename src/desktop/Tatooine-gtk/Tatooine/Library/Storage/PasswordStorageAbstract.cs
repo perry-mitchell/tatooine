4c8b551a08a003004eb2d86f0cc3b28c79bc9c24
Perry Mitchell <perry@perrymitchell.net>
2014-11-16 19:58:57 +0200

    Base application

---
+++ src/desktop/Tatooine-gtk/Tatooine/Library/Storage/PasswordStorageAbstract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Tatooine.IO {
+
+	public abstract class PasswordStorageAbstract {
+		
+		protected string _filename;
+
+		public PasswordStorageAbstract(string filename) {
+			_filename = filename;
+		}
+
+		public abstract Hashtable decodeArchiveData(string data);
+		public abstract string encodeArchiveData(Hashtable data);
+		public abstract Hashtable readArchiveData();
+		public abstract void saveArchiveData(Hashtable data);
+
+	}
+
+}
+
---
+++ src/desktop/common/PasswordFileAbstract.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tatooine.IO {
+
+	abstract class PasswordFileAbstract {
+
+		
+		
+	}
+
+}
