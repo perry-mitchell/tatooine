018b3a87b6c80b2bb8949f828100a8a38c8a415b
Perry Mitchell <perry@kiosked.com>
2014-12-05 09:28:32 +0200

    Groups and entry titles working

--- src/desktop/Tatooine-gtk/Tatooine/ArchiveWindow.cs
+++ src/desktop/Tatooine-gtk/Tatooine/ArchiveWindow.cs
@@ -10,12 +10,15 @@ public partial class ArchiveWindow: Gtk.Window
 	private string archiveFilename;
 	private PasswordArchive archive;
 
+	private List<string> groupHashes;
 	private ListStore groupsListStore;
+	private ListStore entriesListStore;
 
 	public ArchiveWindow (string archiveFilePath, SecureString password) : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
 		archiveFilename = archiveFilePath;
+		groupHashes = new List<string>();
 		bool loaded = false;
 		try {
 			archive = PasswordArchive.createFromFileUsingPassword(archiveFilename, password);
@@ -44,14 +47,34 @@ public partial class ArchiveWindow: Gtk.Window
 		groupNameCol.AddAttribute(groupNameCell, "text", 0);
 		groupsListStore = new ListStore(typeof (string));
 		groupsView.Model = groupsListStore;
+
+		entriesView.AppendColumn("Title", new CellRendererText(), "text", 0);
+
+		entriesListStore = new ListStore(typeof (string));
+		entriesView.Model = entriesListStore;
+
 	}
 
 	protected void populateInterface ()
 	{
 		Dictionary<string, string> groups = archive.getGroups ();
 		Dictionary<string, string>.ValueCollection valueColl = groups.Values;
+		Dictionary<string, string>.KeyCollection keyColl = groups.Keys;
+		groupHashes.Clear ();
 		foreach (string groupName in valueColl) {
-			groupsListStore.AppendValues(groupName);
+			groupsListStore.AppendValues (groupName);
+		}
+		foreach (string groupHash in keyColl) {
+			groupHashes.Add (groupHash);
+		}
+	}
+
+	protected void populateEntriesForGroup (string hash)
+	{
+		entriesListStore.Clear();
+		List<PasswordEntry> entries = archive.getEntriesForGroup (hash);
+		foreach (PasswordEntry pe in entries) {
+			entriesListStore.AppendValues(pe.getTitle());
 		}
 	}
 
@@ -60,5 +83,20 @@ public partial class ArchiveWindow: Gtk.Window
 		Application.Quit ();
 		//a.RetVal = true;
 	}
+
+	protected void groupItemSelected (object o, EventArgs args)
+	{
+		TreeSelection selection = (o as TreeView).Selection;
+		TreeModel model;
+		TreeIter iter;
+
+		if (selection.GetSelected (out model, out iter)) {
+			int index = model.GetPath(iter).Indices[0];
+			string hash = groupHashes[index];
+			populateEntriesForGroup(hash);
+			//Console.WriteLine ("Selected: " + model.GetValue(iter, 0) + " ({0})", index);
+		}
+	}
+
 
 }
--- src/desktop/Tatooine-gtk/Tatooine/gtk-gui/ArchiveWindow.cs
+++ src/desktop/Tatooine-gtk/Tatooine/gtk-gui/ArchiveWindow.cs
@@ -4,12 +4,15 @@
 public partial class ArchiveWindow
 {
 	private global::Gtk.UIManager UIManager;
+	private global::Gtk.Action FileAction;
 	private global::Gtk.VBox vbox1;
 	private global::Gtk.MenuBar menubar1;
 	private global::Gtk.HBox hbox2;
 	private global::Gtk.ScrolledWindow GtkScrolledWindow;
 	private global::Gtk.TreeView groupsView;
 	private global::Gtk.VBox vbox2;
+	private global::Gtk.ScrolledWindow GtkScrolledWindow1;
+	private global::Gtk.TreeView entriesView;
 	private global::Gtk.Statusbar statusbar3;
 	
 	protected virtual void Build ()
@@ -18,6 +21,9 @@ public partial class ArchiveWindow
 		// Widget ArchiveWindow
 		this.UIManager = new global::Gtk.UIManager ();
 		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
+		this.FileAction = new global::Gtk.Action ("FileAction", global::Mono.Unix.Catalog.GetString ("File"), null, null);
+		this.FileAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("File");
+		w1.Add (this.FileAction, null);
 		this.UIManager.InsertActionGroup (w1, 0);
 		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "ArchiveWindow";
@@ -28,7 +34,7 @@ public partial class ArchiveWindow
 		this.vbox1.Name = "vbox1";
 		this.vbox1.Spacing = 6;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString ("<ui><menubar name='menubar1'/></ui>");
+		this.UIManager.AddUiFromString ("<ui><menubar name='menubar1'><menu name='FileAction' action='FileAction'/></menubar></ui>");
 		this.menubar1 = ((global::Gtk.MenuBar)(this.UIManager.GetWidget ("/menubar1")));
 		this.menubar1.Name = "menubar1";
 		this.vbox1.Add (this.menubar1);
@@ -42,6 +48,7 @@ public partial class ArchiveWindow
 		this.hbox2.Spacing = 6;
 		// Container child hbox2.Gtk.Box+BoxChild
 		this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
+		this.GtkScrolledWindow.WidthRequest = 10;
 		this.GtkScrolledWindow.Name = "GtkScrolledWindow";
 		this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
 		// Container child GtkScrolledWindow.Gtk.Container+ContainerChild
@@ -56,21 +63,33 @@ public partial class ArchiveWindow
 		this.vbox2 = new global::Gtk.VBox ();
 		this.vbox2.Name = "vbox2";
 		this.vbox2.Spacing = 6;
+		// Container child vbox2.Gtk.Box+BoxChild
+		this.GtkScrolledWindow1 = new global::Gtk.ScrolledWindow ();
+		this.GtkScrolledWindow1.Name = "GtkScrolledWindow1";
+		this.GtkScrolledWindow1.ShadowType = ((global::Gtk.ShadowType)(1));
+		// Container child GtkScrolledWindow1.Gtk.Container+ContainerChild
+		this.entriesView = new global::Gtk.TreeView ();
+		this.entriesView.CanFocus = true;
+		this.entriesView.Name = "entriesView";
+		this.GtkScrolledWindow1.Add (this.entriesView);
+		this.vbox2.Add (this.GtkScrolledWindow1);
+		global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vbox2 [this.GtkScrolledWindow1]));
+		w6.Position = 0;
 		this.hbox2.Add (this.vbox2);
-		global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox2]));
-		w5.Position = 1;
+		global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.vbox2]));
+		w7.Position = 1;
 		this.vbox1.Add (this.hbox2);
-		global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.hbox2]));
-		w6.Position = 1;
+		global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.hbox2]));
+		w8.Position = 1;
 		// Container child vbox1.Gtk.Box+BoxChild
 		this.statusbar3 = new global::Gtk.Statusbar ();
 		this.statusbar3.Name = "statusbar3";
 		this.statusbar3.Spacing = 6;
 		this.vbox1.Add (this.statusbar3);
-		global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.statusbar3]));
-		w7.Position = 2;
-		w7.Expand = false;
-		w7.Fill = false;
+		global::Gtk.Box.BoxChild w9 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.statusbar3]));
+		w9.Position = 2;
+		w9.Expand = false;
+		w9.Fill = false;
 		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
@@ -78,5 +97,6 @@ public partial class ArchiveWindow
 		this.DefaultWidth = 680;
 		this.DefaultHeight = 443;
 		this.Show ();
+		this.groupsView.CursorChanged += new global::System.EventHandler (this.groupItemSelected);
 	}
 }
