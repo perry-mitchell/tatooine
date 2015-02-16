2b634ce458febb9016236bb5a0a404c58a7ae060
Perry Mitchell <perry@perrymitchell.net>
2015-02-16 22:02:35 +0200

    New entry dialog

---
+++ source/gtk/TatooineDesktop/NewEntryDialog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TatooineDesktop {
+	public partial class NewEntryDialog : Gtk.Dialog {
+		public NewEntryDialog () {
+			this.Build ();
+		}
+	}
+}
+
---
+++ source/gtk/TatooineDesktop/gtk-gui/TatooineDesktop.NewEntryDialog.cs
@@ -0,0 +1,10 @@
+
+namespace TatooineDesktop
+{
+	public partial class NewEntryDialog
+	{
+		private Gtk.Button buttonCancel;
+		
+		private Gtk.Button buttonOk;
+	}
+}
